85a2a286023371624bdb65fc7dce3028e39d1084
raquelscv <raquelscv@gmail.com>
2025-02-13 14:31:57 +0100

    revision joins

--- Consultas_TT.sql
+++ Consultas_TT.sql
@@ -97,6 +97,7 @@ select
 	concat(a.first_name, ' ', a.last_name) as nombre_actor
 from actor a
 join film_actor fa on a.actor_id = fa.actor_id
-join film_category fc on fa.film_id = fc.film_id
+join film f on fa.film_id = f.film_id
+join film_category fc on f.film_id = fc.film_id
 join category c on fc.category_id = c.category_id
 where c.name = 'Sci-Fi';
